46040e2096835d5b5379cce9cc285896e2a89b2d
aikokendo <alejandrameraz@gmail.com>
2017-01-05 15:12:04 +0800

    gibbs recipe

--- gibbs.py
+++ gibbs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import random
+from random import shuffle
 
 class Gibbs:
     def __init__(self):
@@ -7,12 +7,13 @@ class Gibbs:
 
     def gibbs_ask(self,x,e,bn,N):
         #returns an estimate of P(x|e)
-        print(random.shuffle(['a','b','c']))
-
         for i in range(N):
-            print('choose uniformly random non evidence node Zj')
-            print('Set value of Zj in z by sampling from P(Zj|mb(Zj))')
-            print('N[x] = N[x] + 1 where x is the value of X in z')
+            #create uniform random sample to select a Zj per iteration xD
+            x = [i for i in range(10)] #to change to size of BN - size of Evidence
+            shuffle(x)
+            for z in x:
+                print('Set value of Zj in z by sampling from P(Zj|mb(Zj))')
+                print('N[x] = N[x] + 1 where x is the value of X in z')
         print('return normalized(N)')
 
     def mb(self,z,bn):
